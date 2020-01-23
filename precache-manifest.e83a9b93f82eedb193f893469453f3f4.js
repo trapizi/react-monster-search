21ef862906cb55d8866d0cf81b3e70b58d9cf6c5
Steven Ngo <z5076470@students.unsw.edu.au>
2020-01-23 15:52:47 +1100

    Updates

---
+++ precache-manifest.e83a9b93f82eedb193f893469453f3f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ffaa4618040f918a1c403027a44276",
+    "url": "/react-monster-search/index.html"
+  },
+  {
+    "revision": "599b18f7b597ccd45dfe",
+    "url": "/react-monster-search/static/css/main.9c8c85c3.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/react-monster-search/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster-search/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "599b18f7b597ccd45dfe",
+    "url": "/react-monster-search/static/js/main.9ddedb5a.chunk.js"
+  },
+  {
+    "revision": "fd3fef414e65c48e27e4",
+    "url": "/react-monster-search/static/js/runtime-main.49ba6904.js"
+  }
+]);
